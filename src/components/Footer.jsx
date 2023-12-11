f88f75cba133e87edcd7fcf9991eb59df7eadcfe
Kafri <kafri@Prevalensis-MacBook-Air.local>
2023-12-11 10:12:30 +0700

    setup layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,16 +19,10 @@ export function Footer() {
       <Container.Outer>
         <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
           <Container.Inner>
-            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-              <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                <NavLink href="/about">About</NavLink>
-                <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/speaking">Speaking</NavLink>
-                <NavLink href="/uses">Uses</NavLink>
-              </div>
+            <div className="flex flex-col items-center justify-center gap-6 sm:flex-row">
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Spencer Sharp. All rights
-                reserved.
+                &copy; {new Date().getFullYear()} Reminderme - Kafriansyah. All
+                rights reserved.
               </p>
             </div>
           </Container.Inner>
